9be5362fe2b535e103cce497cae2e3fa3b5af146
Ankita <Ankitashejal11@gmail.com>
2024-02-01 18:33:50 +0530

    commit

---
+++ src/app/tab4/tab4.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonicModule,AlertController  } from '@ionic/angular';
+import { AbstractControl,FormBuilder, FormControl, FormGroup, ValidatorFn, Validators  } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-tab4',
+  templateUrl: './tab4.page.html',
+  styleUrls: ['./tab4.page.scss'],
+  standalone: true,
+  imports: [IonicModule, CommonModule, FormsModule]
+})
+export class Tab4Page implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/tabs/tabs.page.ts
+++ src/app/tabs/tabs.page.ts
@@ -1,7 +1,7 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import { homeOutline, peopleOutline, calendarClearOutline } from 'ionicons/icons';
+import { homeOutline, personOutline, calendarClearOutline,clipboardOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tabs',
@@ -14,6 +14,6 @@ export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {
-    addIcons({ homeOutline,peopleOutline,calendarClearOutline});
+    addIcons({ homeOutline,personOutline,calendarClearOutline,clipboardOutline});
   }
 }
